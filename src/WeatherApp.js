cbc3b09e2e758319598460066fb952e83014216b
Albert Arvesu <neldaarvesu@gmail.com>
2022-03-04 21:47:11 +1100

    fix for retrieving country code

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -4,23 +4,27 @@ import LocationForm from './components/LocationForm';
 import Weather from './components/weather/Weather';
 import WeatherAppLoader from './components/WeatherAppLoader';
 
-const DEFAULT_COUNTRY_CODE = 'AU';
 const URL =
   'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=';
-const IPAPI_URL = 'http://ip-api.com/json';
+const IPAPI_URL = 'https://ipinfo.io?token=';
 const RESPONSE_OK = 200;
-const SUCCESS = 'success';
+const COUNTRY_CODE_SZ = 2;
 const MIN_ZIPLENGTH = 4;
 const MAX_ZIPLENGTH = 10;
 
 const WeatherApp = () => {
   const [weatherData, setWeatherData] = useState();
+  const [geoPositionDone, setGeoPositionDone] = useState(false);
   // const [weatherList, setWeatherList] = useEffect([]);
   const [zipCode, setZipCode] = useState('');
-  const [countryCode, setCountryCode] = useState('');
+  const [countryCode, setCountryCode] = useState();
+  const [defaultLocation, setDefaultLocation] = useState({
+    zip: '',
+    country: '',
+  });
 
   const sendAPIRequest = async (posOrZip) => {
-    console.log(posOrZip);
+    console.log('API', posOrZip);
     const apiKey = process.env.REACT_APP_OWMAP_API_KEY;
     const url = `${URL}${apiKey}&${posOrZip}`;
     fetch(url)
@@ -39,45 +43,56 @@ const WeatherApp = () => {
   };
 
   const getIPAPI = () => {
-    fetch(IPAPI_URL)
+    const token = process.env.REACT_APP_IPINFO_TOKEN;
+    console.log(`${IPAPI_URL}${token}`);
+    fetch(`${IPAPI_URL}${token}`)
       .then((response) => response.json())
       .then((data) => {
-        if (data.status === SUCCESS) {
-          setZipCode(data.zip);
-          setCountryCode(data.countryCode);
+        if (data.country.length === COUNTRY_CODE_SZ) {
+          setDefaultLocation({ zip: data.postal, country: data.country });
+          setCountryCode(data.country);
         }
       });
   };
 
-  const getWeatherByZip = () => {
-    sendAPIRequest(`zip=${zipCode},${countryCode}`);
+  const getWeatherByZip = (zip, country) => {
+    console.log(`GET: zip=${zip},${country}`);
+    if (zip.length === 0) return;
+    sendAPIRequest(`zip=${zip},${country}`);
   };
 
   const geoLocSuccess = (pos) => {
     console.log(pos);
     sendAPIRequest(`lat=${pos.coords.latitude}&lon=${pos.coords.longitude}`);
+    setGeoPositionDone(true);
     getIPAPI();
   };
 
   const geoLocError = (err) => {
     console.warn(`ERROR(${err.code}): ${err.message}`);
+    setGeoPositionDone(true);
     getIPAPI();
   };
 
   useEffect(() => {
-    getIPAPI();
     navigator.geolocation.getCurrentPosition(geoLocSuccess, geoLocError);
     console.log(`UseEffect`);
   }, []);
 
   useEffect(() => {
-    getWeatherByZip();
-  }, [countryCode]);
+    console.log('Country feth', defaultLocation.zip);
+    if (weatherData && geoPositionDone) {
+      console.log('Failed to geozips', geoPositionDone);
+      return;
+    }
+    getWeatherByZip(defaultLocation.zip, defaultLocation.country);
+  }, [defaultLocation]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     if (zipCode.length >= MIN_ZIPLENGTH && zipCode.length <= MAX_ZIPLENGTH) {
-      sendAPIRequest(`zip=${zipCode},${countryCode}`);
+      console.log('Submit');
+      getWeatherByZip(zipCode, countryCode);
     } else {
       setZipCode('');
     }
